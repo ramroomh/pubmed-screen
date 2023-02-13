65486a87d51460ead0e23952ebdd688e8d5c81d4
Dave Halliday <hallidave@gmail.com>
2023-02-13 10:31:48 -0500

    Added rate limiter class, updated comments

--- src/pubmed_screen/esearch.py
+++ src/pubmed_screen/esearch.py
@@ -2,60 +2,35 @@ import time
 
 import requests
 
-class SearchResult:
-    """A SearchResult object holds all of the information returned by
-    a PubMed search query."""
-    def __init__(self, query_response):
-        self.request_url = query_response.url
-        self.search_result = query_response.json()["esearchresult"]
-
-    @property
-    def url(self):
-        """URL for the search request"""
-        return self.request_url
-
-    @property
-    def count(self):
-        """Number of PMID results"""
-        return self.search_result["count"]
-    
-    @property
-    def id_list(self):
-        """A list of PMIDs"""
-        return self.search_result["idlist"]
-
-    @property
-    def query_translation(self):
-        """A canonical representation of the search term"""
-        return self.search_result["querytranslation"]
-
-    def __repr__(self):
-        return str(self.search_result)
-
 
 class PubMed:
+    """The PubMed class uses the PubMed ESearch API to retrieve search results.
+
+    The ESearch API is documumented here:
+    https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.ESearch
+
+    There is a list of valid search field tags documented here:
+    https://pubmed.ncbi.nlm.nih.gov/help/#using-search-field-tags
+    """
+
     def __init__(self, api_key=None):
         self.api_key = api_key
         if self.api_key:
             # 10 requests per second with an API key
-            self.min_interval = 0.100
+            self.limiter = RateLimiter(10)
         else:
             # 3 requests per second without an API key
-            self.min_interval = 0.333
-        self.last_query = 0.0
+            self.limiter = RateLimiter(3)
         self.last_url = None
         self.last_search_result = None
 
     def query(self, term, max_results=200):
-        """Returns a SearchResult object for the given search term"""
+        """Returns a SearchResult object for the given search term."""
         if max_results > 10000:
             raise ValueError("max_results cannot be larger than 10000")
 
         # Enforce minimum intervals between calls
-        interval = time.time() - self.last_query
-        if interval < self.min_interval:
-            time.sleep(self.min_interval - interval)
-        self.last_query = time.time()
+        self.limiter.wait()
 
         # Send search request
         url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
@@ -72,6 +47,53 @@ class PubMed:
         self.last_url = response.url
         self.last_search_result = response.json()["esearchresult"]
         return SearchResult(response)
+
+
+class SearchResult:
+    """A SearchResult object holds all of the information returned by
+    a PubMed search query."""
+
+    def __init__(self, query_response):
+        self.request_url = query_response.url
+        self.search_result = query_response.json()["esearchresult"]
+
+    @property
+    def url(self):
+        """URL for the search request"""
+        return self.request_url
+
+    @property
+    def count(self):
+        """Number of PMID results"""
+        return int(self.search_result["count"])
+
+    @property
+    def id_list(self):
+        """A list of PMIDs"""
+        return self.search_result["idlist"]
+
+    @property
+    def query_translation(self):
+        """A canonical representation of the search term"""
+        return self.search_result["querytranslation"]
+
+    def __repr__(self):
+        return str(self.search_result)
+
+
+class RateLimiter:
+    def __init__(self, rate):
+        """The rate parameter is the maximum number of times that wait() can be
+        called in one second."""
+        self.min_interval = 1.0 / rate
+        self.last_wait = 0.0
+
+    def wait(self):
+        """Sleep if the call rate to wait() has been exceeded."""
+        interval = time.time() - self.last_wait
+        if interval < self.min_interval:
+            time.sleep(self.min_interval - interval)
+        self.last_wait = time.time()
 
 
 if __name__ == "__main__":
